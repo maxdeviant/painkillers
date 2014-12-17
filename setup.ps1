c2e84855774dd80b990022e5952ba4a85026173f
Marshall Bowers <elliott.codes@gmail.com>
2014-12-17 08:35:59 -0500

    Use variable substitution in pathnames

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
-$ps_dir = $env:userprofile + '\Documents\WindowsPowerShell'
+$ps_dir = "$env:userprofile\Documents\WindowsPowerShell"
 
 if (!(test-path $ps_dir)) {
     mkdir $ps_dir
 }
 
-cp profile.ps1 ($ps_dir + '\Microsoft.PowerShell_profile.ps1')
+cp profile.ps1 ("$ps_dir\Microsoft.PowerShell_profile.ps1")
