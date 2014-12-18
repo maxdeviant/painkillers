d3ab10cffe11289463025f3b16c2cc433306f67d
Marshall Bowers <elliott.codes@gmail.com>
2014-12-18 15:03:48 -0500

    Add working install script for Sublime Text 3

--- install.ps1
+++ install.ps1
@@ -3,8 +3,7 @@ $packages = @(
         title='Sublime Text 3';
         version='3065';
         url='http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065%20x64%20Setup.exe';
-        arguments='';
-        destination=''
+        arguments='/verysilent /norestart /tasks="contextentry"';
     }
 )
 
@@ -18,5 +17,17 @@ foreach($package in $packages) {
     $name = $package.title
     $version = $package.version
 
+    echo "Downloading $name"
+
     $web.downloadfile($package.url, "$pwd/bin/$name v$version.exe")
+
+    $arguments = $package.arguments
+
+    echo "Installing $name"
+
+    invoke-expression -command "$pwd/bin/'$name v$version.exe' $arguments"
 }
+
+echo "Finished package installations"
+
+rm -recurse -force bin
